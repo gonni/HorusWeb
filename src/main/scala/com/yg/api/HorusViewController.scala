d20cf5ecc3f3590798de0a095013b193a980b3ce
1000074 <a1000074@1000074ui-MacBookPro.local>
2021-12-02 19:13:52 +0900

    daily

--- src/main/scala/com/yg/api/HorusViewController.scala
+++ src/main/scala/com/yg/api/HorusViewController.scala
@@ -1,11 +1,30 @@
 package com.yg.api
 
-import com.yg.data.CrawlSeed
+import com.yg.data._
 import org.scalatra._
+import org.slf4j.LoggerFactory
+import slick.jdbc.MySQLProfile.api._
+//import scala.concurrent.ExecutionContext.Implicits.global
 
-class HorusViewController extends ScalatraServlet {
+trait HorusRoute extends ScalatraBase with FutureSupport {
+  val logger = LoggerFactory.getLogger(getClass)
+
+  def db: Database
 
   get("/seeds") {
+    logger.info("get crawl_unit9")
+    contentType = "text/plain"
+
+    db.run(DbHorus.findAllFromCrawlSeeds.result) map {
+      xs => xs.collect()
+    }
+  }
+}
+
+class HorusViewController(val db: Database) extends ScalatraServlet with FutureSupport with HorusRoute {
+  protected implicit def executor = scala.concurrent.ExecutionContext.Implicits.global
+
+  get("/d/seeds") {
     val data = List(
       CrawlSeed(1L, "Https://www.naver.com", "Naver",  "Good"),
       CrawlSeed(2L, "Https://www.dakao.com", "Daum",  "Good"),
@@ -14,6 +33,4 @@ class HorusViewController extends ScalatraServlet {
 //      com.yg.api.html
       com.yg.api.html.horus.render(data)
   }
-
-
 }
---
+++ src/main/scala/com/yg/data/DbHorus.scala
@@ -0,0 +1,29 @@
+package com.yg.data
+
+import slick.jdbc.MySQLProfile.api._
+object DbHorus {
+  class CrawlSeeds(tag: Tag) extends Table[(Int, String, String, String)](tag, "CRAWL_SEEDS8") {
+    def seedNo = column[Int]("SEED_NO", O.PrimaryKey, O.AutoInc) // This is the primary key column
+    def urlPattern = column[String]("URL_PATTERN")
+    def title = column[String]("TITLE")
+    def status = column[String]("STATUS")
+
+    // Every table needs a * projection with the same type as the table's type parameter
+    def * = (seedNo, urlPattern, title, status)
+  }
+
+  val crawlSeeds = TableQuery[CrawlSeeds]
+
+  val createSchemaAction = (crawlSeeds.schema).create
+
+  val insertCrawlSeed = DBIO.seq(
+    CrawlTables.crawlSeeds.map(c => (c.urlPattern, c.title, c.status))
+      += ("http://test.yg.com", "Test_" + System.currentTimeMillis(), "TEST")
+  )
+
+  val findAllFromCrawlSeeds = {
+    for {
+      ci <- crawlSeeds
+    } yield (ci.seedNo, ci.title, ci.urlPattern, ci.status)
+  }
+}
