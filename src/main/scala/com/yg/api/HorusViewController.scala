cb4ee0636d8e0b851edcc44b43a28fd52fd67304
1000074 <a1000074@1000074ui-MacBookPro.local>
2021-12-03 15:11:18 +0900

    daily commit

--- src/main/scala/com/yg/api/HorusViewController.scala
+++ src/main/scala/com/yg/api/HorusViewController.scala
@@ -18,33 +18,47 @@ trait HorusRoute extends ScalatraBase with FutureSupport {
     logger.info("get crawl_unit9")
 //    contentType = "text/plain"
 
-    var ls = new util.ArrayList[CrawlSeed]()
+//    var ls = new util.ArrayList[CrawlSeed]()
     var lss = ArrayBuffer[CrawlSeed]()
     db.run(DbHorus.findAllFromCrawlSeeds.result) map {
           xs => xs map {
             case(a, b, c, d) => {
-              println("aaa")
-              ls.add(CrawlSeed(a, b, c, d))
+//              println("aaa")
+//              ls.add(CrawlSeed(a, b, c, d))
               lss += CrawlSeed(a, b, c, d)
             }
           }
-
           com.yg.api.html.horus.render(lss)
     }
+  }
+
+  get("/inputSeed") {
+    val title = params("title")
+    val urlPattern = params("urlp")
+    val status = params("status")
+
+
+    val paramMap = request.getParameterMap()
+
+
+    logger.info("Need to Data Add to DB ..")
+//    redirect("/horus/seeds")
+    s"Data($title | $urlPattern | $status) inserted at " + System.currentTimeMillis
+  }
+
+  get("/newSeed") {
+    logger.info("Detected Input Seed Page ..")
+    com.yg.api.html.horusForm()
   }
 }
 
 class HorusViewController(val db: Database) extends ScalatraServlet with FutureSupport with HorusRoute {
+//  val logger2 = LoggerFactory.getLogger(getClass)
   protected implicit def executor = scala.concurrent.ExecutionContext.Implicits.global
 
+
+
   get("/d/seeds") {
-    val data = List(
-      CrawlSeed(1L, "Https://www.naver.com", "Naver",  "Good"),
-      CrawlSeed(2L, "Https://www.dakao.com", "Daum",  "Good"),
-      CrawlSeed(3L, "Https://www.nate.com", "Nate", "Bad"),
-      )
-//      com.yg.api.html
-//      com.yg.api.htmml.horus.render(data)
-    "AB"
+    "AA"
   }
 }
