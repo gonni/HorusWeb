b8e71f39149575de7cd6a0f2b62b43714a0db1b9
Gonni <jeff.yg.kim@gmail.com>
2022-02-04 18:25:48 +0900

    daily commit

--- src/main/scala/com/yg/cwl/CrawlAdminViewController.scala
+++ src/main/scala/com/yg/cwl/CrawlAdminViewController.scala
@@ -5,10 +5,13 @@ import org.scalatra._
 import org.slf4j.LoggerFactory
 import play.twirl.api.Html
 import slick.jdbc.MySQLProfile.api._
+import org.scalatra.forms._
+import org.scalatra.i18n.I18nSupport
 
 import scala.collection.mutable.ArrayBuffer
 
-trait CrawlAdminViewProcessing extends ScalatraServlet with FutureSupport {
+trait CrawlAdminViewProcessing extends ScalatraServlet
+  with FormSupport with I18nSupport with FutureSupport{
   val logger = LoggerFactory.getLogger(getClass)
 
   def db: Database
@@ -31,6 +34,49 @@ trait CrawlAdminViewProcessing extends ScalatraServlet with FutureSupport {
     logger.info("Request create & manage new seed")
     val main = com.yg.cwl.html.seedInput.render()
     layouts.html.dashboard.render("Dashboard Template Main", main)
+  }
+
+  case class ValidateNewSeedForm (
+    crawlTitle: String,
+    urlPattern: String,
+    listUrlPattern: Option[String],
+    listTopAreaFilter: Option[String],
+    contTitleInPage: Option[String],
+    contMainCont: Option[String],
+    contDateOnPage: Option[String]
+  )
+
+  val newSeedForm = mapping(
+    "crawlTitle" -> label("title", text(required, maxlength(10))),
+    "urlPattern" -> label("lblUrlPattern", text(required, maxlength(20))),
+    "listUrlPattern" -> label("", ???),
+    "listTopAreaFilter" -> label("", ???),
+    "contTitleInPage" -> label("", ???),
+    "contMainCont" -> label("", ???),
+    "contDateOnPage" -> label("", ???),
+  )(ValidateNewSeedForm.apply)
+
+  post("/seed/validate") {
+    logger.info("Add new seed action detected ..")
+
+//    validate(newSeedForm)(
+//      errors : Seq[(String, String)] => {
+////        BadRequest(html.error(errors))
+//        ???
+//      },
+//      form : ValidateNewSeedForm => {
+//        ???
+//      }
+//    )
+
+//    validate(newSeedForm)(
+//      hasErrors: Seq[(String, String)] => {
+//        ???
+//      },
+//      successes: ValidateNewSeedForm => {
+//        ???
+//      }
+//    )
   }
 
   get("/main0") {
