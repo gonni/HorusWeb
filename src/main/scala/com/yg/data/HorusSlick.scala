c76ffd06c24bbdef904a48d06595fec9ed2157f1
jeff <gonni21c@gmail.com>
2022-02-08 08:01:41 +0900

    y c

--- src/main/scala/com/yg/data/HorusSlick.scala
+++ src/main/scala/com/yg/data/HorusSlick.scala
@@ -12,6 +12,9 @@ import slick.jdbc.MySQLProfile.api._
 import java.sql.Date
 
 object HorusSlick {
+  case class CrawlSeed(seedNo: Int, urlPattern: String, title: String, status: String)
+  case class WrapperRule()
+
   class CrawlSeeds(tag: Tag)
     extends Table[(Int, String, String, String)](tag, "CRAWL_SEEDS") {
 
@@ -22,6 +25,19 @@ object HorusSlick {
 
     // Every table needs a * projection with the same type as the table's type parameter
     def * = (seedNo, urlPattern, title, status)
+  }
+
+  class CrawlSeedsV2(tag: Tag)
+    extends Table[CrawlSeed](tag, "CRAWL_SEEDS") {
+
+    def seedNo = column[Int]("SEED_NO", O.PrimaryKey, O.AutoInc) // This is the primary key column
+    def urlPattern = column[String]("URL_PATTERN")
+    def title = column[String]("TITLE")
+    def status = column[String]("STATUS")
+
+    // Every table needs a * projection with the same type as the table's type parameter
+    def * =
+      (seedNo, urlPattern, title, status) <> (CrawlSeed.tupled, CrawlSeed.unapply)
   }
 
   class WrapperRules(tag: Tag)
@@ -43,12 +59,16 @@ object HorusSlick {
 
   val a = (for{
     isRes <- tblCrawlSeeds.map(c => (c.title, c.urlPattern, c.status)) += ("AA", "BB", "CC")
-    isRes.
+
   } yield ()).transactionally
 
-  val insertNewSeed = DBIO.seq(
+//  val insertNewSeed = DBIO.seq(
+//
+//  )
+  def db: Database
 
-  )
-
+  def create(title: String, urlPattern: String): Future[Int] = db.run{
+    (tblCrawlSeeds returning tblCrawlSeeds.map(_.seedNo)) += CrawlSeed(0, urlPattern, title, "aa")
+  }
 
 }
