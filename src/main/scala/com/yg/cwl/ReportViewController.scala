84248e653d22ccedab40bf7967b8f3a940e71eb0
김영곤 <jeff.y.kim@onestorecorp.com>
2022-05-12 17:46:33 +0900

    daily commit

--- src/main/scala/com/yg/cwl/ReportViewController.scala
+++ src/main/scala/com/yg/cwl/ReportViewController.scala
@@ -1,17 +1,10 @@
 package com.yg.cwl
 
-import com.yg.data.{CrawlSeed, DbHorus, HorusSlick}
 import org.scalatra._
 import org.slf4j.LoggerFactory
-import play.twirl.api.Html
 import slick.jdbc.MySQLProfile.api._
 import org.scalatra.forms._
 import org.scalatra.i18n.I18nSupport
-
-import scala.:+
-import scala.collection.mutable.ArrayBuffer
-import scala.concurrent.Await
-import scala.concurrent.duration.Duration
 
 trait ReportViewProcessing extends ScalatraServlet
   with FormSupport with I18nSupport with FutureSupport {
@@ -22,7 +15,8 @@ trait ReportViewProcessing extends ScalatraServlet
   get("/rttf") {
     logger.info("Request RT-TF report page ..")
 
-    com.yg.report.html.rttf()
+    val rttf = com.yg.report.html.rttf()
+    layouts.html.dashboard.render("RealTime Term-F Main", rttf)
   }
 }
 
