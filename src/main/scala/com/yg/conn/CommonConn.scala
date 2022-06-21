ab63d1d679ff5db4f243f6461e84e4737eaf6ba7
김영곤 <jeff.y.kim@onestorecorp.com>
2022-06-21 14:13:26 +0900

    daily commit

--- src/main/scala/com/yg/conn/CommonConn.scala
+++ src/main/scala/com/yg/conn/CommonConn.scala
@@ -2,30 +2,56 @@ package com.yg.conn
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
+import akka.http.scaladsl.model._
 import akka.http.scaladsl.model.{ContentTypes, HttpEntity, HttpMethods, HttpRequest, HttpResponse}
-import akka.http.scaladsl.unmarshalling.Unmarshal
-import akka.stream.ActorMaterializer
 
-import java.net.URLEncoder
 import scala.concurrent.duration.DurationInt
-import scala.concurrent.{Await, Future}
-import scala.util.{Failure, Success}
+import scala.concurrent.{Await, ExecutionContextExecutor, Future}
 
-trait CommonConn {
+import scala.concurrent.ExecutionContext.Implicits.global
+
+object CommonConn {
   implicit val system = ActorSystem()
 
-  def writeData(rawData: String): String = {
+  def writeData(url: String, bodyData: String): String = {
     val request = HttpRequest(
       method = HttpMethods.POST,
       //      uri = RuntimeConfig().getString("cpa_admin.cpa_api_write_url"),
-      uri = "http://localhost:8086/write?db=mydb",
-      entity = HttpEntity(
-        rawData
-      )
+      //      uri = "http://localhost:8086/write?db=mydb",
+      uri = url,
+//      headers = List(headers.`Content-Type`.apply(ContentTypes.`application/json`)),
+      entity = HttpEntity(ContentTypes.`application/json`, bodyData)
     )
 
     val res = Await.result(Http().singleRequest(request), 10.seconds)
-    res.status.value
+    val status = res.status.value
+    if(status == "200 OK") {
+      println("Dive into 200 OK")
+      val data = res.entity.dataBytes.map(_.utf8String).runForeach(data => println(data))
+      "Succ"
+    } else {
+      println("Detected Error ..")
+      s"Error: ${status}"
+    }
+  }
+
+  def writeData2(url: String, bodyData: String): String = {
+    val request = HttpRequest(
+      method = HttpMethods.POST,
+      uri = url,
+      entity = HttpEntity(ContentTypes.`application/json`, bodyData)
+    )
+
+    val responseFuture = Http().singleRequest(request)
+    val timeout = 3000.millis
+    val responseAsString = Await.result(
+      responseFuture
+        .flatMap {resp => resp.entity.toStrict(timeout)}
+        .map {strictEntity => strictEntity.data.utf8String},
+      timeout
+    )
+
+    responseAsString
   }
 
 }
--- src/main/scala/com/yg/conn/CrawlCoreClient.scala
+++ src/main/scala/com/yg/conn/CrawlCoreClient.scala
@@ -1,10 +1,12 @@
 package com.yg.conn
 
+import akka.actor.ActorSystem
 import org.json4s.DefaultFormats
 import org.json4s.jackson.JsonMethods.{compact, render}
 import org.json4s.jackson.Serialization.write
 
 object CrawlCoreClient extends App {
+//  implicit val system = ActorSystem()
 
   println("Active JS System ..")
   implicit val formats: DefaultFormats = DefaultFormats
@@ -13,11 +15,21 @@ object CrawlCoreClient extends App {
 //  println(s"jsString -> ${jsString}")
 
   def crawlListUrl(crawlOption : CrawlPageWrapOption) : String = {
+    val body = CrawlPageWrapOption(
+      "https://news.naver.com/main/list.naver?mode=LS2D&sid2=263&sid1=101&mid=sec&listType=title&date=20220621&page=1",
+      "^(https://n.news.naver.com/mnews/).*$",
+      "ul.type02"
+    )
 
+    val jsBody = write(body)
+    println(s"Body data to send -> ${jsBody}")
+    val response = CommonConn.writeData2("http://localhost:8070/crawl/page/list", jsBody)
 
-
-    "TBD"
+    println(s"Response Body -> ${response}")
+    response
   }
+
+  crawlListUrl(null)
 }
 
-case class CrawlPageWrapOption(targetUrl: String, grabUrlPattern: String, grpDomPath: String)
+case class CrawlPageWrapOption(targetSeedUrl: String, filterCrawlUrlRxPattern: String, filterDomGroupAttr: String)
