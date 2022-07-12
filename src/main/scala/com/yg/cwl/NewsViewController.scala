fb2b8da2938716fcde5e98aa32940cd1a2fcd89b
김영곤 <jeff.y.kim@onestorecorp.com>
2022-07-12 18:01:56 +0900

    daily commit

--- src/main/scala/com/yg/cwl/NewsViewController.scala
+++ src/main/scala/com/yg/cwl/NewsViewController.scala
@@ -2,11 +2,14 @@ package com.yg.cwl
 
 import com.yg.api.SlickRoutes
 import com.yg.data.DbHorusCrawled
+import com.yg.data.DbHorusCrawled.CrawlUnit
 import org.scalatra.forms.FormSupport
 import org.scalatra.{FutureSupport, ScalatraServlet}
 import org.slf4j.LoggerFactory
 import org.scalatra.i18n.I18nSupport
 import slick.jdbc.MySQLProfile.api._
+
+import scala.concurrent.Future
 
 trait NewsViewProcessing extends ScalatraServlet
   with FormSupport with I18nSupport with FutureSupport {
@@ -16,14 +19,34 @@ trait NewsViewProcessing extends ScalatraServlet
 
   get("/hot") {
     val seedNo = params("seedNo").toInt
-    logger.info("Request Hot-News {}", seedNo)
-    db.run(DbHorusCrawled.findAll2.result) map {xs =>
-      xs map {
-        row => {
-          println("row " + row)
+    logger.info(" -> Request Hot-News {}", seedNo)
+//    db.run(DbHorusCrawled.findAll(9).map(row => (row.url, row.anchorText)).result) map {
+//      rows => {
+//        rows map {
+//          row => println(row)
+//        }
+//      }
+//    }
+
+//    db.run(DbHorusCrawled.findAll2.result).map {rows => {
+//      rows.map(println)
+//    }}
+
+//    println("-----")
+
+//    db.run(DbHorusCrawled.findAll3(9L).result).map(
+//      println
+//    )
+
+    val res : Future[Seq[CrawlUnit]] = db.run[Seq[CrawlUnit]](DbHorusCrawled.findAll(9).result)
+    res.foreach(a => {
+      a map {
+        ab => {
+          println("===>" + ab)
         }
       }
-    }
+    })
+
 
     val newsPage =com.yg.report.html.news.render()
     layouts.html.dashboard.render("News", newsPage)
--- src/main/scala/com/yg/data/DbHorusCrawled.scala
+++ src/main/scala/com/yg/data/DbHorusCrawled.scala
@@ -1,53 +1,55 @@
 package com.yg.data
 
 import slick.jdbc.MySQLProfile.api._
+
+import java.sql.Timestamp
 import java.time.LocalDateTime
 
 object DbHorusCrawled {
   case class CrawlUnit(
-                      crawlNo: Option[Int],
-                      url: String,
-                      anchorText: String,
-                      anchorImg: String,
-                      status: String,
-                      seedNo: Int,
-                      pageDate: String,
-                      regDate: LocalDateTime,
-                      updDate: LocalDateTime,
-                      pageText: String,
-                      pageTitle: String,
-                      parsedPageDate: LocalDateTime
+                        crawlNo: Long,
+                        url: Option[String],
+                        anchorText: Option[String],
+                        anchorImg: Option[String],
+                        status: Option[String],
+                        seedNo: Long,
+                        pageDate: Option[String],
+                        regDate: Option[Timestamp],
+                        updDate: Option[Timestamp],
+                        pageText: Option[String],
+                        pageTitle: Option[String],
+                        parsedPageDate: Option[Timestamp]
                       )
 
-  class CrawlUnitTableSchema(tag: Tag) extends Table[CrawlUnit](tag, "CRAWL_UNIT1") {
-    def crawlNo = column[Int]("CRAWL_NO", O.PrimaryKey, O.AutoInc)
-    def url = column[String]("URL")
-    def anchorText = column[String]("ANCHOR_TEXT")
-    def anchorImg = column[String]("ANCHOR_IMG")
-    def status = column[String]("STATUS")
-    def seedNo = column[Int]("SEED_NO")
-    def pageDate = column[String]("PAGE_DATE")
-    def regDate = column[LocalDateTime]("REG_DATE")
-    def updDate = column[LocalDateTime]("UPD_DATE")
-    def pageText = column[String]("PAGE_TEXT")
-    def pageTitle = column[String]("PAGE_TITLE")
-    def parsedPageDate = column[LocalDateTime]("PARSED_PAGE_DATE")
+  class CrawlUnitTableSchema(tag: Tag) extends Table[CrawlUnit](tag, None,"CRAWL_UNIT1") {
+    def crawlNo = column[Long]("CRAWL_NO", O.PrimaryKey, O.AutoInc)
+    def url = column[Option[String]]("URL")
+    def anchorText = column[Option[String]]("ANCHOR_TEXT")
+    def anchorImg = column[Option[String]]("ANCHOR_IMG")
+    def status = column[Option[String]]("STATUS")
+    def seedNo = column[Long]("SEED_NO")
+    def pageDate = column[Option[String]]("PAGE_DATE")
+    def regDate = column[Option[Timestamp]]("REG_DATE")
+    def updDate = column[Option[Timestamp]]("UPD_DATE")
+    def pageText = column[Option[String]]("PAGE_TEXT")
+    def pageTitle = column[Option[String]]("PAGE_TITLE")
+    def parsedPageDate = column[Option[Timestamp]]("PARSED_PAGE_DATE")
 
-    def * = (crawlNo.?, url, anchorText, anchorImg, status, seedNo, pageDate, regDate,
+    def * = (crawlNo, url, anchorText, anchorImg, status, seedNo, pageDate, regDate,
       updDate, pageText, pageTitle, parsedPageDate) <> (CrawlUnit.tupled, CrawlUnit.unapply)
   }
 
   val crawlUnitsQuery = TableQuery[CrawlUnitTableSchema]
   val createSchemaAction = (crawlUnitsQuery.schema).create
 
-  def findAll(seedNo1: Int) = crawlUnitsQuery.filter(_.seedNo === seedNo1)
+  def findAll(seedNo: Long) = crawlUnitsQuery.filter(_.seedNo === seedNo)
     .drop(0)
-    .take(25).result
+    .take(20)
 
   val findAll2 = {
     for{
-      cu <- crawlUnitsQuery
-    } yield (cu.crawlNo, cu.url, cu.anchorText)
+      cu <- crawlUnitsQuery.filter(_.seedNo === 9L)
+    } yield (cu.crawlNo, cu.url, cu.anchorText, cu.regDate)
   }
 
 
