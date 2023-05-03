d675d7fc9ef16c8475da1b819e35765d3f89757c
ygkim <gonni21c@gmail.com>
2023-05-03 23:03:33 +0900

    daily commit

--- docker_build.sh
+++ docker_build.sh
@@ -1,3 +1,3 @@
 sbt clean assembly
-docker build --tag horus.web:0.1 .
-docker run -p 18090:8090 -d --name horus.web --net horus_net horus.web:0.1
+docker build --tag horus.web:0.5 .
+docker run -p 18090:8090 -d --name horus.web5 --net horus_net horus.web:0.5
